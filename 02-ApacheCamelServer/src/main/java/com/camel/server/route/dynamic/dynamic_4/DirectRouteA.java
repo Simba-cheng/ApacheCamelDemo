a1e417a064667b5378e1ce367adefa342c09a8dd
Simba-cheng <simba_cheng@163.com>
2018-08-01 23:05:48 +0800

    代码调整、优化

---
+++ 02-ApacheCamelServer/src/main/java/com/camel/server/route/dynamic/dynamic_4/DirectRouteA.java
@@ -0,0 +1,42 @@
+package com.camel.server.route.dynamic.dynamic_4;
+
+import java.io.InputStream;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.io.IOUtils;
+
+public class DirectRouteA extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+
+		from("jetty:http://0.0.0.0:8282/directMain").setExchangePattern(ExchangePattern.InOnly).recipientList().jsonpath("$.data.routeName").delimiter(",").end().process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+
+				Message message = exchange.getIn();
+
+				System.out.println("OtherProcessor中的exchange" + exchange);
+
+				InputStream body = (InputStream) message.getBody();
+				String str = IOUtils.toString(body, "UTF-8");
+
+				System.out.println("OtherProcessor str : " + str);
+
+				// 存入到exchange的out区域
+				if (exchange.getPattern() == ExchangePattern.InOut) {
+					Message outMessage = exchange.getOut();
+					outMessage.setBody(str + " || 被OtherProcessor处理");
+				}
+
+			}
+		});
+
+	}
+
+}
---
+++ 02-ApacheCamelServer/src/main/java/com/camel/server/route/ftp/ftp_1/FTPToLocalRouteBuilder.java
@@ -0,0 +1,19 @@
+package com.camel.server.route.ftp.ftp_1;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * 从FTP服务器上下载文件至本地.
+ * 
+ * @author CYX
+ * @time 2017年12月19日下午8:12:35
+ */
+public class FTPToLocalRouteBuilder extends RouteBuilder {
+
+	// 从FTP上下载文件
+	@Override
+	public void configure() throws Exception {
+		from("ftp://10.0.227.66/?username=cloudsftp&password=cloudsftp&binary=true&passiveMode=true&delete=true&delay=5000").to("file:/temp");
+	}
+
+}
