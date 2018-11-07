6450187a6ca1136b04d940903ef9343054f21128
Simba-cheng <simba_cheng@163.com>
2018-11-07 22:45:28 +0800

    Apache Camel 动态控制路由

---
+++ 14-ApacheCamel-DynamicControlRoutes/src/main/java/com/server/routes/masterroute/MasterHttpRoute.java
@@ -0,0 +1,17 @@
+package com.server.routes.masterroute;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Master路由
+ *
+ * @author CYX
+ * @create 2018-11-07-22:44
+ */
+public class MasterHttpRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
---
+++ 14-ApacheCamel-DynamicControlRoutes/src/main/java/com/server/routes/slaveroute/HttpSlaveRoute.java
@@ -0,0 +1,19 @@
+package com.server.routes.slaveroute;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * 工作节点路由
+ *
+ * @author CYX
+ * @create 2018-11-06-23:03
+ */
+public class HttpSlaveRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+
+
+    }
+}
