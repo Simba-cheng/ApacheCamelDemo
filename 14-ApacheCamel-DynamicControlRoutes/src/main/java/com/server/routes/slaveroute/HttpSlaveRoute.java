4d8a1a91d00dc46e5047d82e7b6437e254583d1b
坚定不移得 <simba_cheng@163.com>
2018-11-08 12:13:36 +0800

    Update HttpSlaveRoute.java
    update

--- 14-ApacheCamel-DynamicControlRoutes/src/main/java/com/server/routes/slaveroute/HttpSlaveRoute.java
+++ 14-ApacheCamel-DynamicControlRoutes/src/main/java/com/server/routes/slaveroute/HttpSlaveRoute.java
@@ -3,17 +3,80 @@ package com.server.routes.slaveroute;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
- * 工作节点路由
+ * 工作路由-Http
  *
  * @author CYX
- * @create 2018-11-06-23:03
+ * @date 2018/11/7 14:03
  */
-public class HttpSlaveRoute extends RouteBuilder {
+public class SlaveHttpRoute extends RouteBuilder {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SlaveHttpRoute.class);
 
     @Override
     public void configure() throws Exception {
 
+        /**
+         * 第一个路由（普通写法，不设置任何配置）
+         */
+        from("jetty:http://127.0.0.1:8282/test-route-control").process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
 
+                InputStream inputStream = exchange.getIn().getBody(InputStream.class);
+                String bodyStr = IOUtils.toString(inputStream, "UTF-8");
 
+                LOGGER.info("bodyStr : {}", new Object[]{bodyStr});
+                inputStream.close();
+
+                // 存入到exchange的out区域
+                if (exchange.getPattern() == ExchangePattern.InOut) {
+                    Message outMessage = exchange.getOut();
+                    outMessage.setBody("消息收到" + "-已处理-slave-01");
+                }
+            }
+        });
+
+        /**
+         * 第二个路由（设置route id）
+         */
+        from("jetty:http://127.0.0.1:8282/test-route-contro2").routeId("test-route-control-02").process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+
+                InputStream inputStream = exchange.getIn().getBody(InputStream.class);
+                String bodyStr = IOUtils.toString(inputStream, "UTF-8");
+
+                LOGGER.info("bodyStr : {}", new Object[]{bodyStr});
+                inputStream.close();
+
+                // 存入到exchange的out区域
+                if (exchange.getPattern() == ExchangePattern.InOut) {
+                    Message outMessage = exchange.getOut();
+                    outMessage.setBody("消息收到" + "-已处理-slave-02");
+                }
+            }
+        });
+
+        /**
+         * 第三个路由（设置route id、设置禁止自动启动）
+         */
+        from("jetty:http://127.0.0.1:8282/test-route-contro3").routeId("test-route-control-03").autoStartup(false).process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+
+                InputStream inputStream = exchange.getIn().getBody(InputStream.class);
+                String bodyStr = IOUtils.toString(inputStream, "UTF-8");
+
+                LOGGER.info("bodyStr : {}", new Object[]{bodyStr});
+                inputStream.close();
+
+                // 存入到exchange的out区域
+                if (exchange.getPattern() == ExchangePattern.InOut) {
+                    Message outMessage = exchange.getOut();
+                    outMessage.setBody("消息收到" + "-已处理-slave-03");
+                }
+            }
+        });
     }
+
 }
