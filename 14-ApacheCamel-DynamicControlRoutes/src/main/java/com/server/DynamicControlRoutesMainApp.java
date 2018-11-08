ede4d156cb8accd417e201a615ff57b920f86149
坚定不移得 <simba_cheng@163.com>
2018-11-08 12:11:55 +0800

    Update DynamicControlRoutesMainApp.java
    update

--- 14-ApacheCamel-DynamicControlRoutes/src/main/java/com/server/DynamicControlRoutesMainApp.java
+++ 14-ApacheCamel-DynamicControlRoutes/src/main/java/com/server/DynamicControlRoutesMainApp.java
@@ -13,18 +13,41 @@ public class DynamicControlRoutesMainApp {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DynamicControlRoutesMainApp.class);
 
-    private static final String LOGBACK_CONF_FILE_PATH = "./conf/logback.xml";
+    private static final String LOGBACK_FILENAME = "./conf/logback.xml";
+
+    public static CamelContext camelContext;
 
     public static void main(String[] args) {
 
-        //加载相关配置，失败直接kill
+        // 初始化参数、异常直接kill程序
         try {
-            LogBackConfigLoader.loadLogBack(LOGBACK_CONF_FILE_PATH);
+            LogBackConfigLoader.loadLogBack(LOGBACK_FILENAME);
         } catch (Exception e) {
+            e.printStackTrace();
             System.exit(1);
         }
 
-        LOGGER.info("123");
+        LOGGER.info("===== 准备启动 CamelContext =====");
 
+        try {
+            // 创建CamelContext上下文
+            camelContext = new DefaultCamelContext();
+            camelContext.start();
+
+            // 将MasterHttpRoute路由加入CamelContext中
+            camelContext.addRoutes(new MasterHttpRoute());
+
+            // 将SlaveHttpRoute路由加入CamelContext中
+            // 其中'test-route-control-03'禁止自动启动，由Master路由进行控制
+            camelContext.addRoutes(new SlaveHttpRoute());
+
+            // 保证主线程不退出
+            synchronized (DynamicControlRoutesMainApp.class) {
+                DynamicControlRoutesMainApp.class.wait();
+            }
+
+        } catch (Exception e) {
+            LOGGER.error("error : {} , errorMessage : {}", new Object[]{e, e.getMessage()});
+        }
     }
 }
