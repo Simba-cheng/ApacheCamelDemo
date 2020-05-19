e998d6c4cc8cb18c7ef6d8d56554e45fe604368c
Simba_cheng <simba_cheng@163.com>
2020-05-19 23:30:20 +0800

    代码优化

--- 01-ApacheCamel-HelloWorld/src/test/java/com/camel/helloworld/TestHttpClient.java
+++ 01-ApacheCamel-HelloWorld/src/test/java/com/camel/helloworld/TestHttpClient.java
@@ -15,14 +15,10 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.log4j.PropertyConfigurator;
 
 public class TestHttpClient {
 
 	public static void main(String[] args) throws Exception {
-
-		PropertyConfigurator.configure("./conf/log4j.properties");
-		PropertyConfigurator.configureAndWatch("./conf/log4j.properties", 1000);
 
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpPost httpPost = new HttpPost("http://127.0.0.1:8282/doHelloWorld");
